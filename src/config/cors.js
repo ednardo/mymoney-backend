f3dc04dd850e948d5d686e74963712e8885afb73
Ednardo Nobre <ednardo.nobre@objective.com.br>
2020-05-28 10:05:20 -0300

    Adicionando opção no cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATH, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers')
     next()
 }
